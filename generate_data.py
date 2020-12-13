c2adb4d6c451b6787df989804df3f09e0dae6466
Recep <recep4sirin@gmail.com>
2020-12-13 21:12:43 +0300

    behavioural fields generated as text

--- generate_data.py
+++ generate_data.py
@@ -12,13 +12,15 @@ industry_type = [
 ]
 
 education_degree = ['Bachelor', 'Master', 'MBA', 'PhD']
+level_of_acceptance = ['Strongly Agree', 'Agree', 'Neutral', 'Disagree', 'Strongly Disagree']
 
 
 def generate_fake_data():
     with open('dataset.csv', 'w', newline='') as file:
         writer = csv.writer(file, delimiter=",")
         writer.writerow(["Gender", "Manager Level", "Title", "Industry Type", "Age", "Education Degree"
-                         "Confident", "Capable", "Efficient", "Intelligent", "Friendly",
+                                                                                     "Confident", "Capable",
+                         "Efficient", "Intelligent", "Friendly",
                          "Trustworthy", "Warm", "Sincere", "Jealous", "Envious", "Admiring", "Proud",
                          "Respectful", "Inspired", "Angry", "Disgusted", "Hateful", "Frustrated", "Ashamed", "Pity",
                          "Sympathy"])
@@ -32,26 +34,26 @@ def generate_fake_data():
                              industry_type[random.randrange(0, len(industry_type))],
                              random.randrange(38, 60),  # age
                              education_degree[random.randrange(0, len(education_degree))],  # education degree
-                             random.randrange(1, 11),  # Confident
-                             random.randrange(1, 11),  # Capable
-                             random.randrange(1, 11),  # Efficient
-                             random.randrange(1, 11),  # Intelligent
-                             random.randrange(1, 11),  # Friendly
-                             random.randrange(1, 11),  # Trustworthy
-                             random.randrange(1, 11),  # Warm
-                             random.randrange(1, 11),  # Sincere
-                             random.randrange(1, 11),  # Jealous
-                             random.randrange(1, 11),  # Envious
-                             random.randrange(1, 11),  # Admiring
-                             random.randrange(1, 11),  # Proud
-                             random.randrange(1, 11),  # Inspired
-                             random.randrange(1, 11),  # Angry
-                             random.randrange(1, 11),  # Disgusted
-                             random.randrange(1, 11),  # Hateful
-                             random.randrange(1, 11),  # Frustrated
-                             random.randrange(1, 11),  # Ashamed
-                             random.randrange(1, 11),  # Pity
-                             random.randrange(1, 11),  # Sympathy
+                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Confident
+                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Capable
+                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Efficient
+                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Intelligent
+                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Friendly
+                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Trustworthy
+                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Warm
+                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Sincere
+                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Jealous
+                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Envious
+                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Admiring
+                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Proud
+                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Inspired
+                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Angry
+                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Disgusted
+                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Hateful
+                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Frustrated
+                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Ashamed
+                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Pity
+                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Sympathy
                              ])
 
 
