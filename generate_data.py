82cf95d3a14d62b59d0fe6afe1c6006ca6c12f38
Recep <recep4sirin@gmail.com>
2020-12-14 21:51:06 +0300

    minor update

--- generate_data.py
+++ generate_data.py
@@ -18,43 +18,38 @@ level_of_acceptance = ['Strongly Agree', 'Agree', 'Neutral', 'Disagree', 'Strong
 def generate_fake_data():
     with open('dataset.csv', 'w', newline='') as file:
         writer = csv.writer(file, delimiter=",")
-        writer.writerow(["Gender", "Manager Level", "Title", "Industry Type", "Age", "Education Degree"
-                                                                                     "Confident", "Capable",
-                         "Efficient", "Intelligent", "Friendly",
-                         "Trustworthy", "Warm", "Sincere", "Jealous", "Envious", "Admiring", "Proud",
-                         "Respectful", "Inspired", "Angry", "Disgusted", "Hateful", "Frustrated", "Ashamed", "Pity",
-                         "Sympathy"])
-        for l in range(1, 2001):
-            if l % 13 == 0:
-                continue
+        writer.writerow(["Gender", "Manager Level", "Title", "Industry Type", "Age", "Education Degree",
+                         "Confident", "Capable", "Efficient", "Intelligent", "Friendly", "Trustworthy", "Warm",
+                         "Sincere", "Jealous", "Envious", "Admiring", "Proud", "Respectful", "Inspired", "Angry",
+                         "Disgusted", "Hateful", "Frustrated", "Ashamed", "Pity", "Sympathy"])
 
-            writer.writerow([gender[random.randrange(0, len(gender))],
-                             manager_level[random.randrange(0, len(manager_level))],
-                             title[random.randrange(0, len(title))],
-                             industry_type[random.randrange(0, len(industry_type))],
-                             random.randrange(38, 60),  # age
-                             education_degree[random.randrange(0, len(education_degree))],  # education degree
-                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Confident
-                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Capable
-                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Efficient
-                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Intelligent
-                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Friendly
-                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Trustworthy
-                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Warm
-                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Sincere
-                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Jealous
-                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Envious
-                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Admiring
-                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Proud
-                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Inspired
-                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Angry
-                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Disgusted
-                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Hateful
-                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Frustrated
-                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Ashamed
-                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Pity
-                             level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Sympathy
-                             ])
+        writer.writerow([gender[random.randrange(0, len(gender))],
+                         manager_level[random.randrange(0, len(manager_level))],
+                         title[random.randrange(0, len(title))],
+                         industry_type[random.randrange(0, len(industry_type))],
+                         random.randrange(38, 60),  # age
+                         education_degree[random.randrange(0, len(education_degree))],  # education degree
+                         level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Confident
+                         level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Capable
+                         level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Efficient
+                         level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Intelligent
+                         level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Friendly
+                         level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Trustworthy
+                         level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Warm
+                         level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Sincere
+                         level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Jealous
+                         level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Envious
+                         level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Admiring
+                         level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Proud
+                         level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Inspired
+                         level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Angry
+                         level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Disgusted
+                         level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Hateful
+                         level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Frustrated
+                         level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Ashamed
+                         level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Pity
+                         level_of_acceptance[random.randrange(0, len(level_of_acceptance))],  # Sympathy
+                         ])
 
 
 if __name__ == "__main__":
