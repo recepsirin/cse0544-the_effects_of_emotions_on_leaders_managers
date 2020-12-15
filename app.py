267dd0a2088125462a353d008c538264c169a751
Recep <recep4sirin@gmail.com>
2020-12-15 22:16:57 +0300

    principal component analysis applied

---
+++ app.py
@@ -0,0 +1,11 @@
+from pca import X_train, X_test
+from scaling import X_test, X_train
+
+# model building process here
+
+# model plotting here if there will be
+
+# model elimination here
+
+if __name__ == "__main__":
+    pass
